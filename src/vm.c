4fe1f461691678403e4821bf51a8f12935fa81ce
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-06-07 14:13:43 -0500

    Finish Compiling Expressions.

--- src/vm.c
+++ src/vm.c
@@ -110,6 +110,19 @@ static InterpretResult run() {
 #pragma GCC diagnostic pop
 
 InterpretResult interpret(const char *source) {
-    compile(source);
-    return run();
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
